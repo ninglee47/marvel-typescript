a2f5f022a7be0c00a6c399166424cb4b6eba178d
ninglee47 <soundmonk0013@gmail.com>
2022-03-25 10:25:30 +0000

    Seed database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,22 @@
 // 1
 import { PrismaClient } from "@prisma/client";
-//let json = require('../lib/Avengers_Disassembled_ready.json');
-//let json = require('../lib/House_of_M_ready.json');
-let json = require('../lib/Civil_War_ready.json');
+let json_Avengers_Disassembled = require('../lib/Avengers_Disassembled_ready.json');
+let json_House_of_M = require('../lib/House_of_M_ready.json');
+let json_civil_war = require('../lib/Civil_War_ready.json');
 
 // 2
 const prisma = new PrismaClient();
 
 // 3
 async function main() {
-    //await prisma.avengers_Disassembled.createMany({
-    //    data: json
-    //});
+    await prisma.avengers_Disassembled.createMany({
+        data: json_Avengers_Disassembled
+    });
     await prisma.civil_War.createMany({
-        data: json
+        data: json_civil_war
+    });
+    await prisma.house_Of_M.createMany({
+        data: json_House_of_M
     });
 }
 
